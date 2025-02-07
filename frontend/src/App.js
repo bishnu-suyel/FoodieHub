6ef432120bcdee97112a6efb3bb9ffd8b2ef2a40
Bishnu Suyel <suyel2007@gmail.com>
2025-02-07 23:11:04 +0200

    Add route for each component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
-// import logo from './logo.svg';
 import './App.css';
-import Receipes from './components/Receipes';
+import Recipes from './components/Recipes'; // Ensure the component name is correct
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Receipes />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Recipes />} />
+        <Route path="/recipes" element={<Recipes />} /> {/* Corrected from "/receipes" to "/recipes" */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
