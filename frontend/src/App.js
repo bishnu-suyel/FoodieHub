1f857f6f8a5cd75e8705622e804e4f0921fa9d45
Bishnu Suyel <suyel2007@gmail.com>
2025-02-08 14:10:46 +0200

    Remove unnecessary commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Recipes from './components/Recipes'; // Ensure the path is correct
-import RecipeDetail from './components/RecipeDetail'; // Ensure the path is correct
+import Recipes from './components/Recipes'; 
+import RecipeDetail from './components/RecipeDetail'; 
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Recipes />} />
-        <Route path="/recipes/:documentId" element={<RecipeDetail />} /> {/* Updated path */}
+        <Route path="/recipes/:documentId" element={<RecipeDetail />} /> 
       </Routes>
     </Router>
   );
